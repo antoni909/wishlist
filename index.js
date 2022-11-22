5129361584c08f00c86bec4f32521e2f4ad0aa7b
antoni <antoni.dev0@gmail.com>
2022-11-22 06:52:01 -0800

    update css styles

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
+
 const form = document.vacationform
 const button = document.getElementById("list-btn")
-
 const div = document.getElementById("div-wishlist")
-
+const oldTitle = document.getElementById("h2-instructions")
 
 button.addEventListener( "click", handleClick )
 
 function handleClick(e){
     e.preventDefault()
-    
     let nodes = []    
     createCardNode( form , nodes )
 
     div.append(...nodes)
+    form.reset()
+    console.log("done")
 }
 
+// TODO - Refactor: hard code, DRY, add helper methods
+// TODO - Replace: use regex for conditional
 function createCardNode(form, nodes){
-    const count = nodes.length
     let placeHolder = "https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg"
-    let { destination,location, photo, description } = form
+    let { destination,location, photo, description } = form``
     placeHolder = ( photo.value < 1 )? placeHolder: photo.value
 
     let newDiv = document.createElement("div")
-    newDiv.setAttribute("class",`new-card-${count}`)
+    newDiv.setAttribute("class","new-card")
+
+    // let newH2Title = document.createElement("h2")
+    // newH2Title.setAttribute("class","h2-new-title")
+    // const newNode = document.createTextNode("My WishList")
+
+    // oldTitle.append(newNode)
+    // oldTitle.replaceChild( oldTitle, newNode )
 
     let editButton = document.createElement("input")
     editButton.setAttribute("type","button")
-    editButton.setAttribute("id","edit-button")
-    editButton.setAttribute("class","edit-button")
-    editButton.setAttribute("name","edit-btn")
+    editButton.setAttribute("class","button-edit")
     editButton.setAttribute("value","Edit")
     
     let removeButton = document.createElement("button")
-    removeButton.setAttribute("id","edit-button")
-    removeButton.setAttribute("name","delete-btn")
-    removeButton.setAttribute("class","button remove-btn")
+    removeButton.setAttribute("class","button-rm")
     removeButton.textContent = "Remove"
 
-    let img = document.createElement( "img")
     let destinationHeading = document.createElement( "h3")
+    destinationHeading.setAttribute("class","dest-header")
     let locationHeading = document.createElement( "h4")
+    locationHeading.setAttribute("class","loc-header")
     let descriptionParagraph = document.createElement( "p")
-
-
+    descriptionParagraph.setAttribute("class","desc-header")
+    
+    let img = document.createElement( "img")
     img.setAttribute("src", placeHolder)
     img.setAttribute("alt", "shows destination")
     destinationHeading.textContent = destination.value
     locationHeading.textContent = location.value
     descriptionParagraph.textContent = description.value
 
-    newDiv.appendChild( img )
-    newDiv.appendChild( destinationHeading )
-    newDiv.appendChild( locationHeading )
-    newDiv.appendChild( descriptionParagraph )
-    newDiv.appendChild( editButton )
-    newDiv.appendChild( removeButton )
-
+    removeButton.addEventListener("click", ()=> div.removeChild(newDiv))
     editButton.addEventListener("click",()=>{
         const updatedDestination = prompt("Enter new name")
         form.destination.value = updatedDestination
@@ -68,14 +69,17 @@ function createCardNode(form, nodes){
         locationHeading.innerText = updatedLocation
         descriptionParagraph.innerText = udpatedPhotoURL
 
+        form.reset()
     })
-    removeButton.addEventListener("click", ()=> div.removeChild(newDiv))
+
+    newDiv.appendChild( img )
+    newDiv.appendChild( destinationHeading )
+    newDiv.appendChild( locationHeading )
+    newDiv.appendChild( descriptionParagraph )
+    newDiv.appendChild( editButton )
+    newDiv.appendChild( removeButton )
 
     nodes.push( newDiv )
-
 }
 
-// loom record screen talk about hmwk/assignment/something where can show code
-// talk through the code you wrote
-// 5 mins total
-
+// 
